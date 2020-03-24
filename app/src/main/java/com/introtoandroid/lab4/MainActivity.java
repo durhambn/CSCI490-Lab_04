73f935ccd81870ec08e52ad0e281e8203da1c027
durhambn <durhambn@g.cofc.edu>
2020-03-23 20:40:37 -0400

    webview lab

--- app/src/main/java/com/introtoandroid/lab4/MainActivity.java
+++ app/src/main/java/com/introtoandroid/lab4/MainActivity.java
@@ -3,6 +3,8 @@ package com.introtoandroid.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = new WebView(this);
+        //webView = (WebView) findViewById(R.id.webview);
+        setContentView(webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://www.google.com");
     }
 }
