fd55f186cb6a859004d449cd768dfe2831e86dfe
durhambn <durhambn@g.cofc.edu>
2020-03-29 12:50:18 -0400

    urlconnection part

--- app/src/main/java/com/introtoandroid/lab4/MainActivity.java
+++ app/src/main/java/com/introtoandroid/lab4/MainActivity.java
@@ -4,11 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void myMethod(String input){
+        URL url = null;
+        try {
+            url = new URL(input);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        URLConnection connection = null;
+        try {
+            connection = url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader inputStreamReader = null;
+        try {
+            inputStreamReader = new InputStreamReader((connection.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String line = null;
+
+
+        try {
+            line = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        int i = 0;
+        while(i < 20) {
+            try {
+                line += bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            i++;
+        }
     }
 }
